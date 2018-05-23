ee2c537f4bd3d4a9130a6bc412ab173c91b9b477
hang.xu <zhiyanzhai563@163.com>
2018-05-23 14:49:30 +0800

    add tv system app seting

---
+++ device/amlogic/p341/vendorsetup.sh
@@ -0,0 +1,6 @@
+add_lunch_combo p341-eng-32
+add_lunch_combo p341-user-32
+add_lunch_combo p341-userdebug-32
+add_lunch_combo p341-eng-64
+add_lunch_combo p341-user-64
+add_lunch_combo p341-userdebug-64
